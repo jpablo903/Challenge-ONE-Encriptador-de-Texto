612aa6f7aecfe45f71e5fbb6b92363e788e78a76
Juan Pablo Rajoy <jpablo_903@hotmail.com>
2023-01-01 19:15:53 -0300

    validacion de campos en el script

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 function encriptar(){
     var texto = document.getElementById("inputTexto").value.toLowerCase();
-    
-    
-    // var texto = document.getElementById("inputTexto").value.toLowerCase();
-    var textoEncriptado = texto.replace(/e/igm,"enter");
-    var textoEncriptado = textoEncriptado.replace(/o/igm,"ober");
-    var textoEncriptado = textoEncriptado.replace(/i/igm,"imes");
-    var textoEncriptado = textoEncriptado.replace(/a/igm,"ai");
-    var textoEncriptado = textoEncriptado.replace(/u/igm,"ufat");
-
-    document.getElementById("imageRight").style.display = "none";
-    document.getElementById("textoTituloMensaje").style.display = "none";
-    document.getElementById("textoEncriptado").innerHTML = textoEncriptado;
-    document.getElementById("copy").style.background = "#D8DFE8";
-    document.getElementById("copy").innerHTML = "¿Copiar?";
-    document.getElementById("copy").style.display = "show";
-    document.getElementById("copy").style.display = "inherit"
+    if (texto == '') {
+        alert('Ingrese un texto para encriptar')
+    } else {
+        var textoEncriptado = texto.replace(/e/igm,"enter");
+        var textoEncriptado = textoEncriptado.replace(/o/igm,"ober");
+        var textoEncriptado = textoEncriptado.replace(/i/igm,"imes");
+        var textoEncriptado = textoEncriptado.replace(/a/igm,"ai");
+        var textoEncriptado = textoEncriptado.replace(/u/igm,"ufat");
+        document.getElementById("imageRight").style.display = "none";
+        document.getElementById("textoTituloMensaje").style.display = "none";
+        document.getElementById("textoEncriptado").innerHTML = textoEncriptado;
+        document.getElementById("copy").style.background = "#D8DFE8";
+        document.getElementById("copy").innerHTML = "¿Copiar?";
+        document.getElementById("copy").style.display = "show";
+        document.getElementById("copy").style.display = "inherit"
+        }
 } 
 
 function desencriptar(){
     var texto = document.getElementById("inputTexto").value.toLowerCase();
+    if(texto == ""){
+        alert('Ingrese un texto para desencriptar')
+    } else {
     var textoEncriptado = texto.replace(/enter/igm,"e");
     var textoEncriptado = textoEncriptado.replace(/ober/igm,"o");
     var textoEncriptado = textoEncriptado.replace(/imes/igm,"i");
@@ -33,6 +36,7 @@ function desencriptar(){
     document.getElementById("copy").innerHTML = "¿Copiar?";
     document.getElementById("copy").style.display = "show";
     document.getElementById("copy").style.display = "inherit"
+    }
 }
 
 function copiar(){
