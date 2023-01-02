208148275de03485fb61b7e5555ab3a9c753be20
Juan Pablo Rajoy <jpablo_903@hotmail.com>
2023-01-02 00:02:29 -0300

    Ajuste de botones encriptar, desencriptar y copiar

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ function encriptar(){
         document.getElementById("imageRight").style.display = "none";
         document.getElementById("textoTituloMensaje").style.display = "none";
         document.getElementById("textoEncriptado").innerHTML = textoEncriptado;
+        document.getElementById("btn-encriptar").innerHTML = "Encriptado";
+        document.getElementById("btn-encriptar").style.fontStyle= "italic";
         document.getElementById("copy").style.background = "#D8DFE8";
+        document.getElementById("copy").style.color = "black";
+        document.getElementById("copy").style.border = "2px solid black";
+        document.getElementById("copy").style.boxShadow = "none";
         document.getElementById("copy").innerHTML = "¿Copiar?";
         document.getElementById("copy").style.display = "show";
         document.getElementById("copy").style.display = "inherit"
@@ -32,7 +37,12 @@ function desencriptar(){
     document.getElementById("imageRight").style.display = "none";
     document.getElementById("textoTituloMensaje").style.display = "none";
     document.getElementById("textoEncriptado").innerHTML = textoEncriptado;
+    document.getElementById("btn-desencriptar").innerHTML = "Desencriptado";
+    document.getElementById("btn-desencriptar").style.fontStyle= "italic";
     document.getElementById("copy").style.background = "#D8DFE8";
+    document.getElementById("copy").style.color = "black";
+    document.getElementById("copy").style.border = "2px solid black";
+    document.getElementById("copy").style.boxShadow = "none";
     document.getElementById("copy").innerHTML = "¿Copiar?";
     document.getElementById("copy").style.display = "show";
     document.getElementById("copy").style.display = "inherit"
@@ -44,7 +54,13 @@ function copiar(){
     textoEncriptadoC.select()
     document.execCommand("copy");
     // alert("COPIADO")
-    document.getElementById("copy").style.background = "greenyellow";
+    document.getElementById("copy").style.color = "#27b305";
+    document.getElementById("copy").style.border = "2px solid #27b305";
+    document.getElementById("copy").style.boxShadow = "2px 2px 10px 5px greenyellow";
     document.getElementById("copy").innerHTML = "Copiado!";
+    document.getElementById("btn-encriptar").innerHTML = "Encriptar";
+    document.getElementById("btn-encriptar").style.fontStyle= "initial";
+    document.getElementById("btn-desencriptar").innerHTML= "Desencriptar";
+    document.getElementById("btn-desencriptar").style.fontStyle= "initial";
 
 }
